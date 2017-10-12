ae6b751289bf45bd5105cb6fb8c88f50ab2ee157
Lancen Daclison <lancenhd@hawaii.edu>
2017-10-12 00:49:18 -1000

    Task10: Methods

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -27,27 +27,22 @@ if(Meteor.isClient){
     },
     'click .increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5} });
+      Meteor.call('updateScore', selectedPlayer, 5);
     },
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update({ _id: selectedPlayer }, {$inc: {score: -5} });
+      Meteor.call('updateScore', selectedPlayer, -5);
     },
     'click .remove': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove({ _id: selectedPlayer });
+      Meteor.call('removePlayer', selectedPlayer);
     }
   });
   Template.addPlayerForm.events({
     'submit form': function(event){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score:0,
-        createdBy: currentUserId
-      });
+      Meteor.call('createPlayer', playerNameVar);
       event.target.playerName.value = "";
     }
 
@@ -62,3 +57,33 @@ if(Meteor.isServer){
     return PlayersList.find({ createdBy: currentUserId });
   });
 }
+
+Meteor.methods({
+  'createPlayer': function(playerNameVar){
+    check(playerNameVar, String);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    }
+  },
+  'removePlayer': function(selectedPlayer){
+    check(selectedPlayer, String);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.remove({ _id: selectedPlayer, createdBy: currentUserId });
+    }
+  },
+  'updateScore': function(selectedPlayer, scoreValue){
+    check(selectedPlayer, String);
+    check(scoreValue, Number);
+    var currentUserId = Meteor.userId();
+    if(currentUserId){
+      PlayersList.update( { _id: selectedPlayer, createdBy: currentUserId },
+          { $inc: {score: scoreValue} });
+    }
+  }
+});
